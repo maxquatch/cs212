124f5cca6dcc53f67773c67dfdef58e1ea94d93f
Max Luttrell <mluttrell@ccsf.edu>
2018-04-06 12:40:47 -0700

    add exercise 11

---
+++ Exercises/Exercise 11 - Closures.playground/Pages/1. Exercise - Create Closures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+ ## Exercise - Create Closures
+ 
+ Create a closure assigned to a constant `blankClosure` that has no parameters and no return value.  It's up to you what to put inside of it, a print statement is a good choice.
+ */
+
+
+/*:
+  Create a closure assigned to a constant `fourClosure` that has no parameters and returns an `Int`.  The body of the closure should always return the value 4. Call the closure two times.  When you call it, make sure you can see that it evaluates to the value 4.
+ */
+
+
+/*:
+ Create a closure assigned to a constant `greeting` that accepts a `name` String argument with no return value.  Within the body of the closure, print the argument.  Call the closure two times using "Gary" and "Linda" as arguments.
+ */
+
+
+//: page 1 of 3  |  [Next: Exercise - Passing Closures as Arguments and Syntactic Sugar](@next)
