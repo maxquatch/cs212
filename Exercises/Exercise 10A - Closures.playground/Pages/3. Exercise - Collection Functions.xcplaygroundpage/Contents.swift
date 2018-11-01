0c1d41273dfc325b691ff5e4c98e23e009872f30
Max Luttrell <mluttrell@ccsf.edu>
2018-11-01 14:25:56 -0700

    add exercise 10A

---
+++ Exercises/Exercise 10A - Closures.playground/Pages/3. Exercise - Collection Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+ ## Exercise - Collection Functions
+ 
+ Using the code below, use the `map` function to create an array of `Int` values, whose values are equal to the original integer value, plus 1.  Use `$0` as you iterate through the values of the array.  Print the resulting collection.
+ */
+
+/*:
+ Using the code below, use the `filter` function to create a new array of `String` values. The new array should only include Strings longer than four characters.  Use `$0` as you iterate through the values of the array.  Print the resulting collection.
+ */
+
+
+//: [Previous](@previous)  |  page 3 of 3
