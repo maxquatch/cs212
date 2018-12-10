eb85e80d0a8b6a3c40b35ff7b0321d1479ed0e77
Max Luttrell <mluttrell@ccsf.edu>
2018-12-10 14:42:03 -0800

    add Exercise 13 solution

---
+++ Exercises/Exercise 13/Solution/Dwarves/Dwarves/DwarfTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DwarfTableViewController.swift
+//  Dwarves
+//
+//  Created by Max Luttrell on 5/4/18.
+//  Copyright © 2018 Max Luttrell. All rights reserved.
+//
+
+import UIKit
+
+class DwarfTableViewController: UITableViewController {
+
+    let dwarves = Dwarves()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // reload saved dwarves
+        dwarves.reload()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    @IBAction func trashDwarves(_ sender: UIBarButtonItem) {
+        dwarves.removeAll()
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func addDwarf(_ sender: UIBarButtonItem) {
+        // add a random dwarf to the model and refresh the table view
+        dwarves.addRandomDwarf()
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dwarfCell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel!.text = dwarves[indexPath.row]
+        
+        return cell
+    }
+    
+
+    
+}
+
