75c22eb3214cc7377328841a12d1099031faa9d1
Max Luttrell <mluttrell@ccsf.edu>
2019-08-14 17:34:55 -0700

    update Homework 1 playground

--- Homework-1.playground/Contents.swift
+++ Homework-1.playground/Contents.swift
@@ -2,12 +2,12 @@
 
 ## 1. Optional String (if statement)
 
- The Swift code below will set variable `password` to something; it might be an actual string, or it might be nil (no value).
+ The Swift code below will set variable `password` to something; it might be an actual string, or it might be nil (no value).  You should be able to see its value on the right side of your playground screen.
  
  If `password` can be safely unwrapped, i.e. it is an actual string, then print the following to the debug area: "xxx is the password" (where xxx is the actual string).
  If `password` cannot be safely unwrapped, then print the following out to the debug area: "there is no password!"
  
- Make sure to try out your code for different values of password -- any time you add or remove code, the Playground should execute again and set password to a new value.  Alternately, you can also do Editor -> Execute Playground to get a new password, or hit the play button towards the bottom of this window.
+ Make sure to try out your code for different values of password -- any time you add or remove code, the Playground should execute again and set password to a new value.  Alternately, you can also do Editor -> Run Playground to get a new password, or hit the play button towards the bottom of this window.
 */
 
 let password: String?
